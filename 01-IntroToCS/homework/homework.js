bcf74c3bb97900b95e8c3fb02f5a4e48af9f8ab5
MemoRodz <gmo.rodriguez@gmail.com>
2022-11-21 15:00:18 -0600

    Update: Homework 01-IntroCS

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,8 +1,33 @@
 'use strict';
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+   let respuesta = 0;
+   for(let i = 0; i < num.length; i++)
+   {
+      respuesta += +num[i] * 2 ** (num.length - 1 - i);
+   }
+   return respuesta;
+}
 
-function DecimalABinario(num) {}
+function DecimalABinario(num) {
+   let respuesta = '';
+   const numeroBits = 3;
+   const bitAbit = [0];
+
+   for (let i = 0; i < numeroBits; i++) {
+       let mascara = 1;
+   
+       const bit = num & (mascara << i);   
+   
+       if(bit === 0) {
+           bitAbit[i] = 0;
+       } else {
+           bitAbit[i] = 1;
+       }
+   }
+   
+   return respuesta = bitAbit.reverse().join('');
+}
 
 module.exports = {
    BinarioADecimal,
