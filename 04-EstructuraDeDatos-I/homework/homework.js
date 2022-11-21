7feb5d35887b5251466ccd47c2dd1bd37b27bdbc
MemoRodz <gmo.rodriguez@gmail.com>
2022-11-21 11:07:37 -0600

    Update: Repository Henry M1 Cohorte 33b

---
+++ 04-EstructuraDeDatos-I/homework/homework.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// EJERCICIO 1
+function nFactorial(n) {}
+
+// EJERCICIO 2
+function nFibonacci(n) {}
+
+// EJERCICIO 3
+function Queue() {}
+
+// No modifiquen nada debajo de esta linea
+// --------------------------------
+
+module.exports = {
+   Queue,
+   nFactorial,
+   nFibonacci,
+};
